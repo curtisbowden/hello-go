94605c5dd1fb11a29c17b72b9ee32dcca24b3072
Curtis Bowden <curtis.bowden@gmail.com>
2018-12-16 17:18:47 -0800

    added random number seed

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
   "fmt"
   "math/rand"
+  "time"
 )
 
 func main() {
+  rand.seed(time.Now())
   fmt.Println("Hello Go!")
   fmt.Println("Go can print random numbers:", rand.Intn(10))
 }
