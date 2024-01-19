19cace5533ec5e6b7cab165f6377a3a69f917d56
Curtis Bowden <curtis.bowden@gmail.com>
2024-01-19 22:57:35 +0000

    did stuff

--- main.go
+++ main.go
@@ -4,10 +4,13 @@ import (
   "fmt"
   "math/rand"
   "time"
+  "github.com/ctomkow/go-hello"
 )
+// go get github.com/ctomkow/go-hello
 
 func main() {
   rand.Seed(time.Now().UnixNano())
+  gohelo.GoHello()
   fmt.Println("Hello Go!")
   fmt.Println("Go can print random numbers:", rand.Intn(100))
   fmt.Printf("")
